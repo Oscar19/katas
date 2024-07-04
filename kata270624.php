6939ac6e9aa80e9666517f1db0c9d1ddc0b91b66
Oscar <oscanguera@gmail.com>
2024-07-04 13:06:22 +0200

    Kata acabada

--- kata270624.php
+++ kata270624.php
@@ -1,23 +1,90 @@
 <?php 
   echo "Escoge la primera fecha \n";
-$dia1 = readline("Dia: \n");
-$mes1 = readline("Mes (En numero): \n");
-$anio1 = readline("Año (ejem: 2024): ");
+  $dia1 = readline("Dia: \n");
+  $mes1 = readline("Mes (En numero): \n");
+  $anio1 = readline("Año (ejem: 2024): ");
 
-echo "Escoge la segunda fecha \n";
-$dia2 = readline("Dia: \n");
-$mes2 = readline("Mes (En numero): \n");
-$anio2 = readline("Año (ejem: 2024): ");
+  echo "Escoge la segunda fecha \n";
+  $dia2 = readline("Dia: \n");
+  $mes2 = readline("Mes (En numero): \n");
+  $anio2 = readline("Año (ejem: 2024): ");
 
-
+//primera forma
     $fecha = new DateTime("$anio1-$mes1-$dia1");
     $fecha1 = new DateTime("$anio2-$mes2-$dia2");
     echo "La fecha es: " . $fecha->format('Y-m-d') . "\n";
     echo "La fecha es: " . $fecha1->format('Y-m-d') . "\n";
 
+    function validarDia($dia, $mes){
+      $diasMes = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+      if ($mes < 1 || $mes > 12) {
+        return false;
+    }
+      if ($dia < 1 || $dia > $diasMes[$mes - 1]) {
+        return false;
+    }
+    return true;
+  }
+
     $diferencia = $fecha->diff($fecha1);
 
-    echo "diferencia: ". $diferencia->format('%R%a dias');
+    echo "diferencia: ". $diferencia->format('%R%a dias')." \n";
+
+
+    // modo molon
+
+    function bisiesto($anio) {
+      return ($anio % 4 == 0 && $anio % 100 != 0) || ($anio % 400 == 0);
+  }
+  function calcularDiasBisiestos($anio) {
+    $count = 0;
+    for ($i = 0; $i <= $anio; $i++) {
+        if (($i % 4 == 0 && $i % 100 != 0) || ($i % 400 == 0)) {
+            $count++;
+        }
+    }
+    return $count;
+  }
+   function diasJisuscrist($anioNew){
+     $aniodelosTiempos = ($anioNew -1) * 365;
+     return $aniodelosTiempos;
+    }
+    function desdeAnionuevo($mes, $anio) {
+      $diasMes = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+      if (bisiesto($anio)) {
+        $diasMes[1] = 29;
+    }
+  
+      $dias = 0;
+      for ($i = 0; $i < $mes - 1; $i++) {
+          $dias += $diasMes[$i];
+      }
+      
+      return $dias;
+  }
+  function calcularAnioCero($dias, $meses, $anios){
+    $hastaJisusCrist = $dias + $meses + $anios;
+    return $hastaJisusCrist;
+  }
+  $aniodelosTiempos1 = diasJisuscrist($anio1) + calcularDiasBisiestos($anio1);
+  $aniodelosTiempos2 = diasJisuscrist($anio2) + calcularDiasBisiestos($anio2);
+  $diasMes1 = desdeAnionuevo($mes1, $anio1);
+  $diasMes2 = desdeAnionuevo($mes2, $anio2);
+
+  $iniciodelostiempos1 = calcularAnioCero($dia1, $diasMes1, $aniodelosTiempos1);
+  $iniciodelostiempos2 = calcularAnioCero($dia2, $diasMes2, $aniodelosTiempos2,);
+
+  $diferencia = $iniciodelostiempos1 - $iniciodelostiempos2;
+
+  echo "la diferencia de dias es de: " .$diferencia . " dias segunda manera \n";
+
+
+
+
+  
+
+    // pasar las fechas al año 0
+    // en este caso 2023 para atras año -1 *365 + meses hasta enero + dias hasta el 1
 
 
 
@@ -25,10 +92,7 @@ $anio2 = readline("Año (ejem: 2024): ");
 
 
 
-/*  $datetime1 = new DateTime('2005-10-11');
-$datetime2 = new DateTime('2009-10-13');
-$interval = $datetime1->diff($datetime2);
-echo $interval->format('%R%a días');*/
+
   
 
 
